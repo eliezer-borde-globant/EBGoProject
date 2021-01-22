acbe3ab6bbe02d15d0c2e07a78080c0b0a27278e
eliezerb7 <eliezer.borde@smiledirectclub.com>
2021-01-22 17:42:09 -0500

    tests finished for services

--- services/service_interfaces.go
+++ services/service_interfaces.go
@@ -1,15 +1,48 @@
 package services
 
 import (
-	"github.com/eliezer-borde-globant/EBGoProject/utils"
+	"context"
+	"encoding/json"
+	"fmt"
+	. "github.com/eliezer-borde-globant/EBGoProject/utils"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/google/go-github/v33/github"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"reflect"
+	"time"
 )
 
 var (
 	//GitServiceObject : Object with service methods.
-	GitServiceObject gitServiceInterface       = gitServiceImplementation{}
-	getRepo          thirdPartyGitHubInterface = thirdPartyGitHubImpl{}
+	GitServiceObject gitServiceInterface = gitServiceImplementation{}
+	ThirdPartyGitHub thirdPartyGitHubInterface = thirdPartyGitHubImpl{}
+)
+
+// Background: Third Party packages.
+var (
+	ContextBackground       = context.Background
+	Oauth2StaticTokenSource = oauth2.StaticTokenSource
+	Oauth2NewClient         = oauth2.NewClient
+	GithubNewClient         = github.NewClient
+	GithubRepositories      = GitServiceObject.GetGitHubClient().Repositories.Get
+	HTTPGetCheckForkedRepo  = http.Get
+	HTTPPostForkRepo        = http.PostForm
+	IoutilReadAll           = ioutil.ReadAll
+	JSONUnmarshal           = json.Unmarshal
+	OsStat                  = os.Stat
+	OsIsNotExist            = os.IsNotExist
+	IoutilWriteFile         = ioutil.WriteFile
+	GitPlainClone           = git.PlainClone
+	OsRemoveAll             = os.RemoveAll
+	ioutilReadFile          = ioutil.ReadFile
+	ReflectValueOf          = reflect.ValueOf
+	JSONMarshalIndent       = json.MarshalIndent
 )
 
 type gitServiceInterface interface {
@@ -18,19 +51,106 @@ type gitServiceInterface interface {
 	CloneRepo(string, string) (*git.Repository, string, error)
 	CreateBranchRepo(repoGit *git.Repository, repoName string, action string) (string, string, error)
 	CreateSecretFile(path string, secretFile string) error
-	EditSecretFile(path string, secretsChanges utils.SecretUpdateMap) error
+	EditSecretFile(path string, secretsChanges SecretUpdateMap) error
 	CreateCommitAndPr(owner string, originalOwner string, repo string, currentBranch string, headBranch string, action string, description string, repoGit *git.Repository) error
 	ForkRepo(owner string, repo string) (forkedOwner interface{}, gitURL interface{}, err error)
 	CheckForkedRepo(url string) error
 }
 
-type gitServiceImplementation struct{}
-type thirdPartyGitHubImpl struct{}
-
-func (service thirdPartyGitHubImpl) RepoGit(repoGit *git.Repository) (*git.Repository, error) {
-	return repoGit, nil
+type thirdPartyGitHubInterface interface {
+	Head(*git.Repository) (*plumbing.Reference, error)
+	Worktree(*git.Repository) (*git.Worktree, error)
+	Fetch(*git.Repository) error
+	Checkout(*git.Worktree, string, *plumbing.Reference) (error,bool)
+	Add(*git.Worktree, string) (plumbing.Hash, error)
+	Commit(*git.Worktree, string, string) (plumbing.Hash, error)
+	CommitObject(*git.Repository, plumbing.Hash) (*object.Commit, error)
+	Push(*git.Repository) error
+	PullRequest(context.Context, *github.Client, string, string, string, string, string, string, string) (*github.PullRequest, *github.Response, error)
 }
 
-type thirdPartyGitHubInterface interface {
-	RepoGit(repoGit *git.Repository) (*git.Repository, error)
+
+type gitServiceImplementation struct {}
+type thirdPartyGitHubImpl struct {}
+
+func (service thirdPartyGitHubImpl) Head(repoGit *git.Repository) (*plumbing.Reference, error) {
+	return repoGit.Head()
+}
+
+func (service thirdPartyGitHubImpl) Worktree(repoGit *git.Repository) (*git.Worktree, error) {
+	return repoGit.Worktree()
+}
+
+func (service thirdPartyGitHubImpl) Fetch(repoGit *git.Repository) error {
+	return repoGit.Fetch(&git.FetchOptions{
+		RefSpecs: []config.RefSpec{"refs/*:refs/*", "HEAD:refs/heads/HEAD"},
+	})
+}
+
+func (service thirdPartyGitHubImpl) Checkout(workingBranch *git.Worktree, branch string, headRef *plumbing.Reference) (error, bool) {
+	var branchExists bool
+	err :=  workingBranch.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.NewBranchReferenceName(branch),
+		Force:  true,
+	})
+	if err == nil {
+		branchExists =  true
+	}
+	if err != nil {
+		ZeroLogger.Info().Msgf("Creating new branch")
+
+		err = workingBranch.Checkout(&git.CheckoutOptions{
+			Hash:   headRef.Hash(),
+			Branch: plumbing.NewBranchReferenceName(branch),
+			Create: true,
+		})
+		if err != nil {
+			branchExists = false
+		}
+		ZeroLogger.Info().Msg("Branch created!")
+	}
+	return err, branchExists
+}
+
+func (service thirdPartyGitHubImpl) Add(workingBranch *git.Worktree, SecretFile string) (plumbing.Hash, error) {
+	return workingBranch.Add(SecretFile)
+}
+
+func (service thirdPartyGitHubImpl) Commit(workingBranch *git.Worktree, msg string, owner string) (plumbing.Hash, error) {
+	return workingBranch.Commit(msg, &git.CommitOptions{
+		Author: &object.Signature{
+			Name: owner,
+			When: time.Now(),
+		},
+	})
+}
+
+func (service thirdPartyGitHubImpl) CommitObject(repoGit *git.Repository, commit plumbing.Hash) (*object.Commit, error) {
+	return repoGit.CommitObject(commit)
+}
+
+func (service thirdPartyGitHubImpl) Push(repoGit *git.Repository) error {
+	obj := &git.PushOptions{}
+	return repoGit.Push(obj)
+}
+
+func (service thirdPartyGitHubImpl) PullRequest(
+	ctx context.Context,
+	client *github.Client,
+	originalOwner string,
+	repo string,
+	action string,
+	owner string,
+	currentBranch string,
+	headBranch string,
+	description string) (*github.PullRequest, *github.Response, error) {
+	newPR := &github.NewPullRequest{
+		Title:               github.String(fmt.Sprintf("[Detect Secrets] %s Secret BaseLine File", action)),
+		Head:                github.String(fmt.Sprintf("%s:%s", owner, currentBranch)),
+		Base:                github.String(headBranch),
+		Body:                github.String(description),
+		MaintainerCanModify: github.Bool(true),
+	}
+
+	return client.PullRequests.Create(ctx, originalOwner, repo, newPR)
 }
--- services/services.go
+++ services/services.go
@@ -1,35 +1,18 @@
 package services
 
 import (
-	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"net/url"
 	"os"
-	"reflect"
 	"strings"
-	"time"
 
 	. "github.com/eliezer-borde-globant/EBGoProject/utils"
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/google/go-github/v33/github"
 	"golang.org/x/oauth2"
 )
 
-// Background: Third Party packages.
-var (
-	ContextBackground       = context.Background
-	Oauth2StaticTokenSource = oauth2.StaticTokenSource
-	Oauth2NewClient         = oauth2.NewClient
-	GithubNewClient         = github.NewClient
-	GithubRepositories      = GitServiceObject.GetGitHubClient().Repositories.Get
-)
 
 func (gitService gitServiceImplementation) GetGitHubClient() *github.Client {
 	ctx := ContextBackground()
@@ -42,27 +25,28 @@ func (gitService gitServiceImplementation) GetGitHubClient() *github.Client {
 
 func (gitService gitServiceImplementation) CheckUserAccessRepo(owner string, repo string) (*github.Repository, error) {
 	ZeroLogger.Info().Msgf("check user has access to %s/%s", owner, repo)
-	ctx := context.Background()
+	ctx := ContextBackground()
 	repoInfo, _, err := GithubRepositories(ctx, owner, repo)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println(err)
 	return repoInfo, nil
 }
 
 func (gitService gitServiceImplementation) CloneRepo(owner string, repo string) (*git.Repository, string, error) {
 	path := fmt.Sprintf("/tmp/%s-%s", owner, repo)
-
 	ZeroLogger.Info().Msgf("Creating folder to clone %s", path)
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
-		err := os.RemoveAll(path)
+	if _, err := os.Stat(path); !OsIsNotExist(err) {
+		ZeroLogger.Error().Msgf("Hola")
+		err := OsRemoveAll(path)
 		if err != nil {
 			ZeroLogger.Error().Msgf("Error path to clone repo from %s/%s, error: %v", owner, repo, err)
 			return nil, "", err
 		}
 	}
 	ZeroLogger.Info().Msg("Starting to clone Repo")
-	repoInfo, err := git.PlainClone(path, false, &git.CloneOptions{
+	repoInfo, err := GitPlainClone(path, false, &git.CloneOptions{
 		URL:      fmt.Sprintf("https://%s@github.com/%s/%s", GitHubToken, owner, repo),
 		Progress: os.Stdout,
 	})
@@ -76,57 +60,43 @@ func (gitService gitServiceImplementation) CloneRepo(owner string, repo string) 
 
 func (gitService gitServiceImplementation) CreateBranchRepo(repoGit *git.Repository, repoName string, action string) (string, string, error) {
 	ZeroLogger.Info().Msgf("Creating Branch to update secret file in repo %s", repoName)
-	headRef, err := repoGit.Head()
+	headRef, err := ThirdPartyGitHub.Head(repoGit)
 	if err != nil {
 		ZeroLogger.Error().Msgf("Error Creating Branch to update secret file in repo %s, error: %v", repoName, err)
 		return "", "", err
 	}
 	headBranchName := strings.ReplaceAll(headRef.Name().String(), "refs/heads/", "")
 	branch := fmt.Sprintf("secret_scanner_api/%s/%s/secrets_baseline_file", repoName, action)
-	workingBranch, err := repoGit.Worktree()
+	workingBranch, err := ThirdPartyGitHub.Worktree(repoGit)
 	if err != nil {
 		ZeroLogger.Error().Msgf("Error Creating Branch to update secret file in repo %s, error: %v", repoName, err)
 		return "", "", err
 	}
 	ZeroLogger.Info().Msgf("Fetching all Branches from %s", repoName)
-	err = repoGit.Fetch(&git.FetchOptions{
-		RefSpecs: []config.RefSpec{"refs/*:refs/*", "HEAD:refs/heads/HEAD"},
-	})
+	err = ThirdPartyGitHub.Fetch(repoGit)
 	if err != nil {
 		ZeroLogger.Error().Msgf("Error fetching remote Branches from repo %s, error: %v", repoName, err)
 		return "", "", err
 	}
 	ZeroLogger.Info().Msgf("Checking if the branch %s exists in %s", branch, repoName)
-	err = workingBranch.Checkout(&git.CheckoutOptions{
-		Branch: plumbing.NewBranchReferenceName(branch),
-		Force:  true,
-	})
-	if err == nil {
+	err, branchExists := ThirdPartyGitHub.Checkout(workingBranch, branch, headRef)
+	if branchExists {
 		ZeroLogger.Info().Msgf("Branch %s already exists in %s, Checking out...", branch, repoName)
 	} else {
-		ZeroLogger.Info().Msgf("Creating new branch %s in %s", branch, repoName)
-		err = workingBranch.Checkout(&git.CheckoutOptions{
-			Hash:   headRef.Hash(),
-			Branch: plumbing.NewBranchReferenceName(branch),
-			Create: true,
-		})
-		if err != nil {
 			ZeroLogger.Error().Msgf("Error Creating Branch to update secret file in repo %s, error: %v", repoName, err)
 			return "", "", err
-		}
-		ZeroLogger.Info().Msgf("Branch created in (%s) with the name (%s)", repoName, branch)
 	}
 	return branch, headBranchName, err
 }
 
 func (gitService gitServiceImplementation) CreateSecretFile(path string, secretFile string) error {
 	ZeroLogger.Info().Msg(fmt.Sprintf("Creating Path %s to add %s file ", path, SecretsFileName))
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		ZeroLogger.Error().Msgf("Error Creating Path %s, error: %v", path, err)
+	if _, err := OsStat(path); OsIsNotExist(err) {
+		ZeroLogger.Error().Msgf("*** Error Creating Path %s, error: %v", path, err)
 		return err
 	}
 	path = fmt.Sprintf("%s/%s", path, SecretsFileName)
-	err := ioutil.WriteFile(path, []byte(secretFile), 0644)
+	err := IoutilWriteFile(path, []byte(secretFile), 0644)
 	ZeroLogger.Info().Msgf("File was created with the content at path: '%s'", path)
 	if err != nil {
 		ZeroLogger.Error().Msgf("Error creating %s file in, %s error: %v", SecretsFileName, path, err)
@@ -138,13 +108,15 @@ func (gitService gitServiceImplementation) CreateSecretFile(path string, secretF
 func (gitService gitServiceImplementation) EditSecretFile(path string, secretsChanges SecretUpdateMap) error {
 	ZeroLogger.Info().Msgf("Starting to edit the secret file at path: '%s'", path)
 	path = fmt.Sprintf("%s/%s", path, SecretsFileName)
-	dat, err := ioutil.ReadFile(path)
+	dat, err := ioutilReadFile(path)
 	if err != nil {
+		ZeroLogger.Info().Msgf("Error..")
 		return err
 	}
 	var fileStruct map[string]interface{}
-	err = json.Unmarshal(dat, &fileStruct)
+	err = JSONUnmarshal(dat, &fileStruct)
 	if err != nil {
+		ZeroLogger.Info().Msgf("Error getting json from existing file..")
 		return err
 	}
 	results, ok := fileStruct["results"].(map[string]interface{})
@@ -153,6 +125,7 @@ func (gitService gitServiceImplementation) EditSecretFile(path string, secretsCh
 		ZeroLogger.Error().Msgf("Error: %v", err)
 		return err
 	}
+	ZeroLogger.Info().Msgf("loop..")
 	for filename, secretData := range secretsChanges {
 		_, ok := results[filename]
 		if !ok {
@@ -160,28 +133,28 @@ func (gitService gitServiceImplementation) EditSecretFile(path string, secretsCh
 		}
 		for _, secret := range secretData {
 			fileData := results[filename]
-			fileSecrets := reflect.ValueOf(fileData)
+			fileSecrets := ReflectValueOf(fileData)
 			for i := 0; i < fileSecrets.Len(); i++ {
 				value := fileSecrets.Index(i)
 				secrets := value.Interface().(map[string]interface{})
 				if secret["hashed_secret"] == secrets["hashed_secret"] && secret["line_number"] == secrets["line_number"] {
 					secrets["is_secret"] = secret["is_secret"]
-					value.Set(reflect.ValueOf(secrets))
+					value.Set(ReflectValueOf(secrets))
 				}
 			}
 			results[filename] = fileSecrets.Interface()
 		}
 	}
 	fileStruct["results"] = results
-	file, parseError := json.MarshalIndent(fileStruct, "", "  ")
+	file, parseError := JSONMarshalIndent(fileStruct, "", "  ")
 	if parseError != nil {
 		ZeroLogger.Error().Msgf("Cannot indent content of the file : %v", parseError)
 		return parseError
 	}
-	writeFileError := ioutil.WriteFile(path, file, 0644)
+	writeFileError := IoutilWriteFile(path, file, 0644)
 	if writeFileError != nil {
 		ZeroLogger.Error().Msgf("Error writing file: %v", parseError)
-		return parseError
+		return writeFileError
 	}
 	return nil
 }
@@ -190,30 +163,25 @@ func (gitService gitServiceImplementation) CreateCommitAndPr(owner string, origi
 	ZeroLogger.Info().Msg("Getting current branch")
 	ZeroLogger.Info().Msgf("Head Branch: %s", headBranch)
 	ZeroLogger.Info().Msgf("Current Branch: %s", currentBranch)
-	workingBranch, err := repoGit.Worktree()
+	workingBranch, err := ThirdPartyGitHub.Worktree(repoGit)
 	if err != nil {
 		ZeroLogger.Info().Msgf("Error getting current branch '%s/%s'", owner, repo)
 		return err
 	}
 	ZeroLogger.Info().Msgf("Adding %s file to new branch ", SecretsFileName)
-	_, err = workingBranch.Add(SecretsFileName)
+	_, err = ThirdPartyGitHub.Add(workingBranch, SecretsFileName)
 	if err != nil {
 		return err
 	}
 	ZeroLogger.Info().Msgf("%s was added to stage ", SecretsFileName)
 	ZeroLogger.Info().Msg("Committing Changes")
-	commit, err := workingBranch.Commit(fmt.Sprintf("chore: %s secret baseline file", action), &git.CommitOptions{
-		Author: &object.Signature{
-			Name: owner,
-			When: time.Now(),
-		},
-	})
+	commit, err := ThirdPartyGitHub.Commit(workingBranch, fmt.Sprintf("chore: %s secret baseline file", action), owner)
 	if err != nil {
 		ZeroLogger.Error().Msgf("Error Committing changes: %v", err)
 		return err
 	}
 	ZeroLogger.Info().Msg("Changes were committed")
-	_, err = repoGit.CommitObject(commit)
+	_, err = ThirdPartyGitHub.CommitObject(repoGit, commit)
 	if err != nil {
 		ZeroLogger.Error().Msgf("Error Committing: %v", err)
 		return err
@@ -221,22 +189,16 @@ func (gitService gitServiceImplementation) CreateCommitAndPr(owner string, origi
 	ZeroLogger.Info().Msgf("Commit created in '%s/%s'", owner, repo)
 
 	ZeroLogger.Info().Msg("Pushing changes to remote")
-	obj := &git.PushOptions{}
-	err = repoGit.Push(obj)
+	err = ThirdPartyGitHub.Push(repoGit)
 	if err != nil {
+		ZeroLogger.Info().Msg("Pushing changes to branch failed")
 		return err
 	}
 	ZeroLogger.Info().Msgf("Branch was pushed '%s/%s'", owner, repo)
 	githubClient := GitServiceObject.GetGitHubClient()
-	newPR := &github.NewPullRequest{
-		Title:               github.String(fmt.Sprintf("[Detect Secrets] %s Secret BaseLine File", action)),
-		Head:                github.String(fmt.Sprintf("%s:%s", owner, currentBranch)),
-		Base:                github.String(headBranch),
-		Body:                github.String(description),
-		MaintainerCanModify: github.Bool(true),
-	}
-
-	_, _, err = githubClient.PullRequests.Create(context.Background(), originalOwner, repo, newPR)
+	_, _, err = ThirdPartyGitHub.PullRequest(
+		ContextBackground(), githubClient, originalOwner, repo, action,
+		owner, currentBranch, headBranch, description)
 	if err != nil {
 		ZeroLogger.Info().Msgf("PR success Updated! '%s/%s'", owner, repo)
 		return err
@@ -248,47 +210,39 @@ func (gitService gitServiceImplementation) CreateCommitAndPr(owner string, origi
 func (gitService gitServiceImplementation) ForkRepo(owner string, repo string) (forkedOwner interface{}, gitURL interface{}, err error) {
 	ZeroLogger.Info().Msgf("Forking repo from '%s/%s'", owner, repo)
 	forkURL := fmt.Sprintf("https://%s@api.github.com/repos/%s/%s/forks", GitHubToken, owner, repo)
-	resp, errPost := http.PostForm(forkURL, url.Values{})
-
+	resp, errPost := HTTPPostForkRepo(forkURL, url.Values{})
 	if errPost != nil {
 		ZeroLogger.Error().Msgf("Error forking Repo from '%s/%s' ", owner, repo)
 		return "", "", errPost
 	}
-
-	body, errBody := ioutil.ReadAll(resp.Body)
-
+	body, errBody := IoutilReadAll(resp.Body)
 	if errBody != nil {
 		ZeroLogger.Error().Msgf("Error forking Repo from '%s/%s', error %s ", owner, repo, errBody)
 		return "", "", errBody
 	}
-
 	ZeroLogger.Info().Msgf("Status Code after forking the repo: %d", resp.StatusCode)
 	formatBody := string(body)
-
 	var result = forkResponseParams{}
-
-	parseError := json.Unmarshal([]byte(formatBody), &result)
+	parseError := JSONUnmarshal([]byte(formatBody), &result)
 	if parseError != nil {
 		ZeroLogger.Error().Msgf("Contents seems to be incorrect in the JSON received: %v", parseError)
 		return nil, nil, parseError
 	}
 	ZeroLogger.Info().Msgf("User who forked: %s", result.Owner.Login)
-
 	return result.Owner.Login, result.GitURL, err
 }
 
 func (gitService gitServiceImplementation) CheckForkedRepo(getURL string) error {
 	ZeroLogger.Info().Msgf("Checking if repo was forked properly")
 	for {
-		response, err := http.Get(getURL)
+		response, err := HTTPGetCheckForkedRepo(getURL)
 		if response.StatusCode == 200 {
 			ZeroLogger.Info().Msgf("Repo has been forked successfully")
-			break
+			return nil
 		}
-		if err != nil {
+		if err != nil || response.StatusCode != 200 {
 			ZeroLogger.Error().Msgf("Error: %v", err)
 			return err
 		}
 	}
-	return nil
 }
